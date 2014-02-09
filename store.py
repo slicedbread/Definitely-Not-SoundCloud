53012ce06223b4a09183f887d6b978ea543d8e2a
Greedoid <jasieni2@illinois.edu>
2014-02-08 20:17:09 -0600

    some more stuff

--- store.py
+++ store.py
@@ -1,5 +1,6 @@
 import pylast
 import config
+import db
 
 # You have to have your own unique two values for API_KEY and API_SECRET
 # Obtain yours from http://www.last.fm/api/account for Last.fm
@@ -15,14 +16,20 @@ network = pylast.LastFMNetwork(api_key = API_KEY, api_secret =
 
 f = open ('songs.txt', 'r')
 
+
+
 for item in f:
 	splits = item.split('--')[1].split('-')
-	track = network.get_track(splits[1], splits[2])
+	#print splits
+	track = network.get_track(splits[0], splits[1])
+	print track.get_mbid()
+#	album = track.get_album()
+#	print album
+#	year = album.get_release_date()
+#	print year
 
-track = network.get_track("Iron Maiden", "The Nomad")
 
 f.close()
-p.close()
 
 # type help(pylast.LastFMNetwork) or help(pylast) in a python interpreter to get more help
 # about anything and see examples of how it works
